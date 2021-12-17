7573f1a6f10d8f281973b6dd3c60e4a5b981f872
Mark Stott <mypantsareontheoutside@gmail.com>
2021-12-17 22:48:49 +0000

    Add scaffolding for the main page.

---
+++ src/Stott.Optimizely.Csp/Presentation/CspAdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stott.Optimizely.Csp.Presentation
+{
+    public class CspAdminController : Controller
+    {
+        [HttpGet]
+        [Authorize(Roles = "CmsAdmin,WebAdmins,Administrators")]
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Stott.Optimizely.Csp/Presentation/CspAdminMenuProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using EPiServer.Authorization;
+using EPiServer.Shell.Navigation;
+
+namespace Stott.Optimizely.Csp.Presentation
+{
+    [MenuProvider]
+    public class RobotsAdminMenuProvider : IMenuProvider
+    {
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            var listMenuItem = new UrlMenuItem("CSP", "/global/cms/admin/stott.optimizely.csp", "/CspAdmin/Index")
+            {
+                IsAvailable = context => true,
+                SortIndex = SortIndex.Last + 1,
+                AuthorizationPolicy = CmsPolicyNames.CmsAdmin
+            };
+
+            return new List<MenuItem> { listMenuItem };
+        }
+    }
+}
