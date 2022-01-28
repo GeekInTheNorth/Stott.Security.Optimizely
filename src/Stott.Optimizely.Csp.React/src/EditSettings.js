e80e58bbc59ce41ae0bce545ddf645c8b024797f
Mark Stott <mypantsareontheoutside@gmail.com>
2022-01-28 22:36:00 +0000

    Update Edit Settings UI to require CSP to be active for Report Only mode to be set.

--- src/Stott.Optimizely.Csp.React/src/EditSettings.js
+++ src/Stott.Optimizely.Csp.React/src/EditSettings.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 
 function EditSettings() {
 
@@ -7,27 +7,37 @@ function EditSettings() {
     const [isCspReportOnly, setIsCspReportOnly] = useState(true);
     const [disableSaveButton, setDisableSaveButton] = useState(true);
 
-    const handleIsCspEnabledChange = (event) => { setIsCspEnabled(event.target.checked); setDisableSaveButton(false); }
-    const handleIsCspReportOnly = (event) => { setIsCspReportOnly(event.target.checked); setDisableSaveButton(false); }
+    const handleIsCspEnabledChange = (event) => { 
+        setIsCspEnabled(event.target.checked); 
+        setIsCspReportOnly(event.target.checked & isCspReportOnly);
+        setDisableSaveButton(false); 
+    }
+
+    const handleIsCspReportOnly = (event) => {
+        setIsCspReportOnly(event.target.checked & isCspEnabled);
+        setDisableSaveButton(false); 
+    }
 
     const handleSaveSettings = (event) => {
         event.preventDefault();
     }
 
     return (
-        <Form>
-            <Form.Group className='my-3'>
-                <Form.Check type='switch' label="Active" checked={isCspEnabled} onChange={handleIsCspEnabledChange} />
-                <div class="form-text">Enabling the Content Security Policy will apply the header to content pages only.</div>
-            </Form.Group>
-            <Form.Group className='my-3'>
-                <Form.Check type='switch' label="Report Only Mode" checked={isCspReportOnly} onChange={handleIsCspReportOnly} />
-                <div class="form-text">Only report violations of the Content Security Policy.</div>
-            </Form.Group>
-            <Form.Group>
-                <Button type='submit' disabled={disableSaveButton} onClick={handleSaveSettings}>Save Changes</Button>
-            </Form.Group>
-        </Form>
+        <Container fluid='md'>
+            <Form>
+                <Form.Group className='my-3'>
+                    <Form.Check type='switch' label="Active" checked={isCspEnabled} onChange={handleIsCspEnabledChange} />
+                    <div class="form-text">Enabling the Content Security Policy will apply the header to content pages only.</div>
+                </Form.Group>
+                <Form.Group className='my-3'>
+                    <Form.Check type='switch' label="Report Only Mode" checked={isCspReportOnly} onChange={handleIsCspReportOnly} />
+                    <div class="form-text">Only report violations of the Content Security Policy.</div>
+                </Form.Group>
+                <Form.Group>
+                    <Button type='submit' disabled={disableSaveButton} onClick={handleSaveSettings}>Save Changes</Button>
+                </Form.Group>
+            </Form>
+        </Container>
     )
 }
 
