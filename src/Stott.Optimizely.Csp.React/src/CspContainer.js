98f5811dc2197037290079cf22e2df5198221394
Mark Stott <mypantsareontheoutside@gmail.com>
2022-02-06 16:10:33 +0000

    Add initial UI for handling legacy headers.

--- src/Stott.Optimizely.Csp.React/src/CspContainer.js
+++ src/Stott.Optimizely.Csp.React/src/CspContainer.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
 import PermissionList from './PermissionList';
 import EditSettings from './EditSettings';
+import EditLegacyHeaderSettings from './EditLegacyHeaderSettings'
 
 function CspContainer() {
 
     return (
-        <Tabs defaultActiveKey='csp-source' id='uncontrolled-tab-example' className='mb-2'>
-            <Tab eventKey='csp-settings' title='Settings'>
+        <Tabs defaultActiveKey='csp-settings' id='uncontrolled-tab-example' className='mb-2'>
+            <Tab eventKey='csp-settings' title='CSP Settings'>
                 <EditSettings></EditSettings>
             </Tab>
-            <Tab eventKey='csp-source' title='Sources'>
+            <Tab eventKey='csp-source' title='CSP Sources'>
                 <PermissionList></PermissionList>
+            </Tab>
+            <Tab eventKey='legacy-headers' title='Security Headers'>
+                <EditLegacyHeaderSettings></EditLegacyHeaderSettings>
             </Tab>
         </Tabs>
     )
