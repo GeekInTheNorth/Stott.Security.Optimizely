d7b33630bfec18bec7a0c6a9e51e12ade6228862
Mark Stott <mypantsareontheoutside@gmail.com>
2022-03-09 22:04:45 +0000

    Initial hooking up of a view component to handle CSP violation events.

---
+++ src/Stott.Optimizely.Csp/Features/Reporting/CspReportingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stott.Optimizely.Csp.Features.Reporting
+{
+    public class CspReportingController : Controller
+    {
+        [HttpPost]
+        [Route("[controller]/[action]")]
+        public IActionResult Report([FromBody]ReportModel cspReport)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+        }
+    }
+}
