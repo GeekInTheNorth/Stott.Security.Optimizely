1e00052f0110f214b645a07fd4f0cd7533aab946
Mark Stott <mypantsareontheoutside@gmail.com>
2022-10-19 23:15:47 +0100

    Add user to the audit log entry and wrap with error handling.

--- src/Stott.Security.Optimizely/Entities/CspDataContext.cs
+++ src/Stott.Security.Optimizely/Entities/CspDataContext.cs
@@ -5,15 +5,28 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
+using Stott.Security.Optimizely.Common;
 using Stott.Security.Optimizely.Features.Audit;
 
 public class CspDataContext : DbContext, ICspDataContext
 {
-    public CspDataContext(DbContextOptions<CspDataContext> options) : base(options)
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    private readonly ILogger<CspDataContext> _logger;
+
+    public CspDataContext(
+        DbContextOptions<CspDataContext> options,
+        IHttpContextAccessor httpContextAccessor,
+        ILogger<CspDataContext> logger)
+        : base(options)
     {
+        _httpContextAccessor = httpContextAccessor;
+        _logger = logger;
     }
 
     public DbSet<CspSettings> CspSettings { get; set; }
@@ -35,7 +48,14 @@ public class CspDataContext : DbContext, ICspDataContext
     {
         if (ChangeTracker.HasChanges())
         {
-            AuditRecords();
+            try
+            {
+                AuditRecords();
+            }
+            catch(Exception exception)
+            {
+                _logger.LogError(exception, $"{CspConstants.LogPrefix} Failed to create audit entry records");
+            }
         }
 
         return base.SaveChangesAsync(cancellationToken);
@@ -43,6 +63,7 @@ public class CspDataContext : DbContext, ICspDataContext
 
     public void AuditRecords()
     {
+        var user = _httpContextAccessor.HttpContext.User.Identity.Name ?? "System";
         var auditTime = DateTime.UtcNow;
         var entries = ChangeTracker.Entries<IAuditableEntity>().ToList();
         foreach (var entry in entries)
@@ -59,6 +80,7 @@ public class CspDataContext : DbContext, ICspDataContext
                         RecordType = recordType,
                         OperationType = state,
                         Actioned = auditTime,
+                        ActionedBy = user,
                         Field = property.Metadata.Name,
                         OldValue = property.OriginalValue?.ToString(),
                         NewValue = property.CurrentValue?.ToString()
