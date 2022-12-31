27b223d2a01c0c095e5605d9e81f8fa32ee50764
Mark Stott <mypantsareontheoutside@gmail.com>
2022-12-31 14:52:58 +0000

    Use embed blocks for test cases rather than hard coded examples on the homepage.

---
+++ Sample/OptimizelyTwelveTest/Features/Blocks/Embed/EmbedBlockViewComponent.cs
@@ -0,0 +1,13 @@
+﻿namespace OptimizelyTwelveTest.Features.Blocks.Embed;
+
+using EPiServer.Web.Mvc;
+
+using Microsoft.AspNetCore.Mvc;
+
+public class EmbedBlockViewComponent : BlockComponent<EmbedBlock>
+{
+    protected override IViewComponentResult InvokeComponent(EmbedBlock currentContent)
+    {
+        return View(currentContent);
+    }
+}
