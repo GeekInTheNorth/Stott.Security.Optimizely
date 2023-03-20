c1db452bf2b78a60fcc42ddac11895f20704db94
Mark Stott <mypantsareontheoutside@gmail.com>
2023-03-20 23:18:45 +0000

    #96 Group sources by primary domain and type on the source list.

--- src/Stott.Security.Optimizely/Features/Permissions/List/CspPermissionListModel.cs
+++ src/Stott.Security.Optimizely/Features/Permissions/List/CspPermissionListModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace Stott.Security.Optimizely.Features.Permissions.List;
 
 using System;
+using System.Linq;
 
+using Newtonsoft.Json;
+
+using Stott.Security.Optimizely.Common;
 using Stott.Security.Optimizely.Entities;
 
 public sealed class CspPermissionListModel
@@ -12,11 +16,16 @@ public sealed class CspPermissionListModel
 
     public string Directives { get; set; }
 
+    [JsonIgnore]
+    internal string SortSource { get; set; }
+
     public CspPermissionListModel(CspSource cspSource)
     {
         Id = cspSource.Id;
         Source = cspSource.Source ?? string.Empty;
         Directives = cspSource.Directives ?? string.Empty;
+
+        SortSource = GetSortSource(Source);
     }
 
     public CspPermissionListModel(string? source, string? directives)
@@ -24,5 +33,40 @@ public sealed class CspPermissionListModel
         Id = Guid.Empty;
         Source = source ?? string.Empty;
         Directives = directives ?? string.Empty;
+
+        SortSource = GetSortSource(Source);
+    }
+
+    private static string GetSortSource(string? source)
+    {
+        try
+        {
+            var sortSource = source ?? string.Empty;
+
+            if (CspConstants.AllSources.Contains(sortSource))
+            {
+                return sortSource;
+            }
+            
+            if (sortSource.Contains('*'))
+            {
+                sortSource = sortSource.Replace("*", "0");
+            }
+
+            if (Uri.IsWellFormedUriString(sortSource, UriKind.Absolute))
+            {
+                var host = new Uri(sortSource).Host;
+                var components = host.Split('.');
+                var primaryDomain = components.Length > 2 ? string.Join(".", components.Skip(components.Length - 2)) : host;
+
+                return $"{primaryDomain} : {sortSource}";
+            }
+
+            return sortSource;
+        }
+        catch(Exception) 
+        {
+            return source ?? string.Empty;
+        }
     }
 }
