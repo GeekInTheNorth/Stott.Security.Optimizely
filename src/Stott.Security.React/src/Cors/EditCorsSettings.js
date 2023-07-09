896358a82961ed9ef98f2bf12f9a7d7a254f4a9e
Mark Stott <mypantsareontheoutside@gmail.com>
2023-07-09 22:28:51 +0100

    Add UI for adding Allowed Origins.

--- src/Stott.Security.React/src/Cors/EditCorsSettings.js
+++ src/Stott.Security.React/src/Cors/EditCorsSettings.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import EditCorsHeader from './EditCorsHeader';
+import EditCorsOrigin from './EditCorsOrigin';
 
 function EditCorsSettings(props) {
 
     const [isCorsEnabled, setIsCorsEnabled] = useState(false);
     const [isAllowCredentials, setIsAllowCredentials] = useState(false);
-    const [isAllowAllMethods, setIsAllowAllMethods] = useState(false);
+    const [isAllowAllMethods, setIsAllowAllMethods] = useState(true);
     const [isAllowGetMethods, setIsAllowGetMethods] = useState(false);
     const [isAllowHeadMethods, setIsAllowHeadMethods] = useState(false);
     const [isAllowPostMethods, setIsAllowPostMethods] = useState(false);
@@ -17,6 +18,7 @@ function EditCorsSettings(props) {
     const [isAllowOptionsMethods, setIsAllowOptionsMethods] = useState(false);
     const [isAllowTraceMethods, setIsAllowTraceMethods] = useState(false);
     const [allowedHeaders, setAllowedHeaders] = useState(["Test", "Test-Two"]);
+    const [allowedOrigins, setAllowedOrigins] = useState(["www.test.com", "www.example.com"]);
     const [disableSaveButton, setDisableSaveButton] = useState(true);
 
     const handleIsCorsEnabledChange = (event) => {
@@ -133,9 +135,6 @@ function EditCorsSettings(props) {
         setDisableSaveButton(false);
     }
 
-    const handleShowSuccessToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(true, title, description);
-    const handleShowFailureToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(false, title, description);
-
     const renderAllowedHeaders = () => {
         return allowedHeaders && allowedHeaders.map((header, index) => {
             return (
@@ -143,6 +142,38 @@ function EditCorsSettings(props) {
             )
         })
     }
+
+    const handleRemoveAllowedOrigin = (origin) => {
+        var newAllowedOrigins = allowedOrigins.filter(function (e) { return e !== origin });
+        setAllowedOrigins(newAllowedOrigins);
+        setDisableSaveButton(false);
+    };
+
+    const handleUpdateAllowedOrigin = (index, newOrigin) => {
+        var newAllowedOrigins = allowedOrigins.map(x => x);
+        newAllowedOrigins[index] = newOrigin;
+
+        setAllowedOrigins(newAllowedOrigins);
+        setDisableSaveButton(false);
+    };
+
+    const handleAddAllowedOrigin = () => {
+        var newAllowedOrigins = allowedOrigins.map(x => x);
+        newAllowedOrigins.push("");
+        setAllowedOrigins(newAllowedOrigins);
+        setDisableSaveButton(false);
+    }
+
+    const renderAllowedOrigins = () => {
+        return allowedOrigins && allowedOrigins.map((origin, index) => {
+            return (
+                <EditCorsOrigin key={index} originIndex={index} originUrl={origin} handleDeleteOrigin={handleRemoveAllowedOrigin} handleUpdateOrigin={handleUpdateAllowedOrigin}></EditCorsOrigin>
+            )
+        })
+    }
+
+    const handleShowSuccessToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(true, title, description);
+    const handleShowFailureToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(false, title, description);
 
     return (
         <Container fluid='md'>
@@ -152,8 +183,10 @@ function EditCorsSettings(props) {
                     <div className='form-text'>Enabling the Cross-Origin Resource Sharing will apply the CORS headers to all requests from both content routes and CMS backend routes.</div>
                 </Form.Group>
                 <Form.Group className='my-3'>
-                    <Form.Check type='switch' label='Allow Credentials.' checked={isAllowCredentials} onChange={handleIsAllowCredentials} />
-                    <div className='form-text'>Allows client side code to access a response when making a request where credentials are to be included.</div>
+                    <Form.Label id='lblAllowedHttpOrigins'>Allowed Origins:</Form.Label>
+                    <p><Button variant='success' type="button" onClick={handleAddAllowedOrigin} className='fw-bold'>+</Button></p>
+                    {renderAllowedOrigins()}
+                    <div className='form-text'>The Origins that may be allowed to make a web request to this service. If no origins are provided here, then all origins will be considered as allowed.</div>
                 </Form.Group>
                 <Form.Group className='my-3'>
                     <Form.Label id='lblRequireCredentials'>Allowed HTTP Methods:</Form.Label>
@@ -175,6 +208,10 @@ function EditCorsSettings(props) {
                     {renderAllowedHeaders()}
                     <div className='form-text'>The headers that may be allowed within a web request. If no headers are provided here, then all headers will be considered as allowed.</div>
                     <div className='form-text'>Please note that 'Accept', 'Accept-Language', 'Content-Language' and 'Content-Type' are considered as safe headers and do not need to be defined here.</div>
+                </Form.Group>
+                <Form.Group className='my-3'>
+                    <Form.Check type='switch' label='Allow Credentials.' checked={isAllowCredentials} onChange={handleIsAllowCredentials} />
+                    <div className='form-text'>Allows client side code to access a response when making a request where credentials are to be included.</div>
                 </Form.Group>
                 <Form.Group className='my-3'>
                     <Button type='submit' disabled={disableSaveButton} onClick={handleSaveSettings}>Save Changes</Button>
