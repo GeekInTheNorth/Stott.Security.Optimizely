ca0722b64b491f4ed859e4ffdd9ee4edf140c9bd
Mark Stott <mypantsareontheoutside@gmail.com>
2024-03-11 23:46:22 +0000

    197 Add a new extension method for safely updating response headers.

---
+++ src/Stott.Security.Optimizely.Test/Extensions/HeaderDictionaryExtensionsTestCases.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Net.Http.Headers;
+
+using NUnit.Framework;
+
+namespace Stott.Security.Optimizely.Test.Extensions;
+
+public static class HeaderDictionaryExtensionsTestCases
+{
+    public static IEnumerable<TestCaseData> ValidHeaderTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(HeaderNames.CacheControl, "public, max-age=31557600");
+            yield return new TestCaseData(HeaderNames.AccessControlAllowHeaders, "*");
+        }
+    }
+
+    public static IEnumerable<TestCaseData> InvalidHeaderTestCases
+    {
+        get
+        {
+            yield return new TestCaseData(" ", " ");
+            yield return new TestCaseData(" ", string.Empty);
+            yield return new TestCaseData(" ", null);
+            yield return new TestCaseData(string.Empty, " ");
+            yield return new TestCaseData(string.Empty, string.Empty);
+            yield return new TestCaseData(string.Empty, null);
+            yield return new TestCaseData(null, " ");
+            yield return new TestCaseData(null, string.Empty);
+            yield return new TestCaseData(null, null);
+        }
+    }
+}
