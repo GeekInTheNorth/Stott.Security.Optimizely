970346b2717ca9c354446bc508ced370f8aa95f7
Mark Stott <mypantsareontheoutside@gmail.com>
2024-03-18 23:23:55 +0000

    Add the ability to download the exported settings.

--- src/Stott.Security.React/src/Tools/ExportSettings.js
+++ src/Stott.Security.React/src/Tools/ExportSettings.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 import { Button } from 'react-bootstrap';
 
 function ExportSettings(props) {
 
+    const handleShowFailureToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(false, title, description);
+
+    const downloadFile = ({ data, fileName, fileType }) => {
+        // Create a blob with the data we want to download as a file
+        const blob = new Blob([data], { type: fileType });
+        // Create an anchor element and dispatch a click event on it
+        // to trigger a download
+        const a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent('click', {
+          view: window,
+          bubbles: true,
+          cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+      }
+
+    const getSettings = async button => {
+        button.preventDefault();
+
+        await axios
+            .get(process.env.REACT_APP_TOOLS_EXPORT)
+            .then((response) => {
+                downloadFile({
+                    data: response.data,
+                    fileName: 'stott-security-settings.json',
+                    fileType: 'text/json',
+                  });
+            },
+            () => {
+                handleShowFailureToast("Error", "Failed to retrieve settings to export.");
+            });
+      }
+
     return (
         <div>
             <label className='mx-3'>Export all CSP, CORS and other security headers.</label>
-            <Button variant='success'>Export</Button>
+            <Button variant='success' onClick={getSettings}>Export</Button>
         </div>
     )
 }
