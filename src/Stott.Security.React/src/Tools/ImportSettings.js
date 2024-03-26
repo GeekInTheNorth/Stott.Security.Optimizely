36982c113776c90346ee3d6f96ddbdfdff6319cf
Mark Stott <mypantsareontheoutside@gmail.com>
2024-03-26 23:16:02 +0000

    Put the import settings file control into a modal with a import and cancel option.

--- src/Stott.Security.React/src/Tools/ImportSettings.js
+++ src/Stott.Security.React/src/Tools/ImportSettings.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
+import { Modal, Button } from "react-bootstrap";
 import axios from 'axios';
 
 function ImportSettings(props) {
 
-  const [fileUploadValue, setFileUploadValue] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [uploadedFile, setUploadedFile] = useState(null);
 
   const handleFileChange = async (e) => {
     if (e.target.files && e.target.files[0]) {
-      var parsedJson = await readJsonFile(e.target.files[0]);
+      setUploadedFile(e.target.files[0]);
+    }
+  };
+
+  const handleSubmitFile = async () => {
+    if (uploadedFile !== null){
+      var parsedJson = await readJsonFile(uploadedFile);
 
       axios.post(process.env.REACT_APP_TOOLS_IMPORT, parsedJson)
-           .then(() => { handleShowSuccessToast("Settings Import", "Settings have been successfully imported."); },
-                 () => { handleShowFailureToast("Settings Import", "Failure encountered importing Settings."); });
+        .then(() => { handleShowSuccessToast("Settings Import", "Settings have been successfully imported."); setShowModal(false); },
+              () => { handleShowFailureToast("Settings Import", "Failure encountered importing Settings."); setShowModal(false); });
     }
-
-    setFileUploadValue(null);
-  };
+  }
 
   const readJsonFile = (file) =>
     new Promise((resolve, reject) => {
@@ -30,14 +36,33 @@ function ImportSettings(props) {
       fileReader.readAsText(file);
     });
 
+  const handleOpenModal = () => { setShowModal(true); };
+  const handleCloseModal = () => { setShowModal(false); };
   const handleShowSuccessToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(true, title, description);
   const handleShowFailureToast = (title, description) => props.showToastNotificationEvent && props.showToastNotificationEvent(false, title, description);
 
   return (
-    <div className='my-4'>
-      <label className='form-label'>Import all CSP, CORS and other security headers.</label>
-      <input id='uploadSettings' type='file' accept='application/json' className='form-control' onChange={handleFileChange} value={fileUploadValue} />
-    </div>
+    <>
+      <div className='my-4'>
+        <label className='form-label'>Import all CSP, CORS and other security headers.</label><br/>
+        <Button variant='success' onClick={handleOpenModal}>Import</Button>
+      </div>
+      <Modal show={showModal} onHide={handleCloseModal} size='lg'>
+        <Modal.Header closeButton className="py-2">
+          <Modal.Title>Import Settings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <label className='form-label'>Select a settings file to upload.</label>
+            <input id='uploadSettings' type='file' accept='application/json' className='form-control' onChange={handleFileChange} />
+          </div>
+          <div className='my-3 text-end'>
+            <Button variant='success' onClick={handleSubmitFile} className='me-3'>Import</Button>
+            <Button variant='danger' onClick={handleCloseModal}>Cancel</Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
 
