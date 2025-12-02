9272b5500bb775f073a3b5674b97bd2e072eff93
Mark Stott <mypantsareontheoutside@gmail.com>
2025-12-02 15:24:19 +0000

    Fix up style imports.

--- src/Stott.Security.Ui/src/main.jsx
+++ src/Stott.Security.Ui/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
