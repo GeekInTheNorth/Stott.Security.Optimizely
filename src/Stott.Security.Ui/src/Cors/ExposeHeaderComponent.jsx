dc27465c16404280ec31fb2c7c8f8453338508e9
Mark Stott <mypantsareontheoutside@gmail.com>
2025-12-03 22:41:31 +0000

    Add function for Content Definition Expose headers

--- src/Stott.Security.Ui/src/Cors/ExposeHeaderComponent.jsx
+++ src/Stott.Security.Ui/src/Cors/ExposeHeaderComponent.jsx
@@ -26,13 +26,22 @@ function ExposeHeaderComponent (props) {
         newHttpHeaders.push({ id: crypto.randomUUID(), value: '' });
         setHttpHeaders(newHttpHeaders);
         handleParentSave(newHttpHeaders);
-    }
+    };
 
     const handleAddContentDeliveryApiHeaders = () => {
+        const contentDeliveryHeaders = [ 'x-epi-contentguid', 'x-epi-branch', 'x-epi-siteid', 'x-epi-startpageguid', 'x-epi-remainingroute', 'x-epi-contextmode' ];
+        handleAddHeaders(contentDeliveryHeaders);
+    };
+
+    const handleAddContentDefinitionApiHeaders = () => {
+        const contentDefinitionHeaders = [ 'x-epi-continuation' ];
+        handleAddHeaders(contentDefinitionHeaders);
+    };
+
+    const handleAddHeaders = (headersToAdd) => {
         var newHttpHeaders = httpHeaders.map(x => x);
         
-        const contentDeliveryHeaders = [ 'x-epi-contentguid', 'x-epi-branch', 'x-epi-siteid', 'x-epi-startpageguid', 'x-epi-remainingroute', 'x-epi-contextmode' ];
-        contentDeliveryHeaders.forEach(cdHeader => {
+        headersToAdd.forEach(cdHeader => {
             if (!headerExists(cdHeader)) {
                 newHttpHeaders.push({ id: crypto.randomUUID(), value: cdHeader });
             }
@@ -40,11 +49,11 @@ function ExposeHeaderComponent (props) {
 
         setHttpHeaders(newHttpHeaders);
         handleParentSave(newHttpHeaders);
-    }
+    };
 
     const headerExists = (headerName) => {
         return httpHeaders.find(h => h.value === headerName) !== undefined;
-    }
+    };
 
     const renderAllowedHeaders = () => {
         return httpHeaders && httpHeaders.map((header) => {
@@ -52,15 +61,15 @@ function ExposeHeaderComponent (props) {
                 <EditCorsHeader key={header.id} headerId={header.id} headerName={header.value} handleDeleteHeader={handleRemoveHttpHeader} handleUpdateHeader={handleUpdateHttpHeader}></EditCorsHeader>
             )
         })
-    }
+    };
 
     const setUpState = () => {
         setHttpHeaders(props.headers);
-    }
+    };
 
     useEffect(() => {
         setUpState()
-    }, [props.headers])
+    }, [props.headers]);
 
     return (
         <Form.Group className='my-3'>
@@ -68,7 +77,8 @@ function ExposeHeaderComponent (props) {
             {renderAllowedHeaders()}
             <p>
                 <Button variant='success' type='button' onClick={handleAddHttpHeader} className='fw-bold me-2'>Add Header</Button>
-                <Button variant='primary' type='button' onClick={handleAddContentDeliveryApiHeaders} className='fw-bold'>Add Content Delivery API Headers</Button>
+                <Button variant='outline-primary' type='button' onClick={handleAddContentDeliveryApiHeaders} className='fw-bold me-2'>Add Content Delivery API Headers</Button>
+                <Button variant='outline-primary' type='button' onClick={handleAddContentDefinitionApiHeaders} className='fw-bold'>Add Content Definition API Headers</Button>
             </p>
             <div className='form-text'>Configures the 'Access-Control-Expose-Headers' header which will be sent from this webserver to the browser to instruct the browser which headers may be exposed to scripts running within the browser when making a cross-origin request. If no headers are provided here, then all headers will be considered as allowed.</div>
             <div className='form-text'>Please note that 'Cache-Control', 'Content-Language', 'Content-Length', 'Content-Type', 'Expires', 'Last-Modified' and 'Pragma' are considered as safe headers and do not need to be defined here.</div>
