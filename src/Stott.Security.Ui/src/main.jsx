7750107c3f2a2c3489b716df9e0e6627fdf41016
Mark Stott <mypantsareontheoutside@gmail.com>
2025-12-03 12:41:15 +0000

    Update build to copy the assets.

--- src/Stott.Security.Ui/src/main.jsx
+++ src/Stott.Security.Ui/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('security-app')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
